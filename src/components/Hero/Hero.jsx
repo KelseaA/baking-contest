6911f8e525cede6ba4b883e8ee8fd0e18cac07e5
Kelsea <andersonkelsea@gmail.com>
2022-03-25 14:05:20 -0500

    update baking contest to cupcake decorating contest

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,8 +7,8 @@ const Hero = () => {
         <div className="Hero__text">
           <h1>The Great Bright Cellars Bake Off</h1>
           <p>
-            Bright Cellars is holding a spring-themed baking contest for the
-            month of April!
+            Bright Cellars is holding a spring-themed cupcake decorating contest
+            for the month of April!
           </p>
         </div>
       </div>
